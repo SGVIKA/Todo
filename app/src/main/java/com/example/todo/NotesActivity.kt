afcd2b7d4c5d1b3cfffff25a21f7f292a36d8475
SGVIKA <sadser.vika@gmail.com>
2023-05-02 21:54:27 +0300

    база данных

--- app/src/main/java/com/example/todo/NotesActivity.kt
+++ app/src/main/java/com/example/todo/NotesActivity.kt
@@ -1,12 +1,11 @@
-    package com.example.todo
+package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.ListFragment
 
-    class MainActivity : AppCompatActivity() {
+class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_notes)
     }
 }
